f81f891f20b197a47a5fc7b4674980f42fd11e59
Prashant M <pmathapa@mathworks.com>
2021-05-04 19:17:00 +0530

    Adding tested libraries

---
+++ TestApp/Lib/TestLib.h
@@ -0,0 +1,13 @@
+/*
+ * TestLib.h
+ *
+ *  Created on: May 4, 2021
+ *      Author: pmathapa
+ */
+
+#ifndef TESTLIB_H_
+#define TESTLIB_H_
+
+int Addition(int a, int b);
+
+#endif /* TESTLIB_H_ */
--- TestApp/TestApp.c
+++ TestApp/TestApp.c
@@ -5,10 +5,16 @@
  *      Author: pmathapa
  */
 #include <stdio.h>
+#include "TestLib.h"
 
 int main ( void )
 {
+	int res;
+
 	printf("Hellow World! \n");
+	res = Addition(5, 6);
+
+	printf("%d \n", res);
 
 	return 0;
 }
